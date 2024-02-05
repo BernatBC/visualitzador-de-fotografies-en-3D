a00b5482806eb38eeccd8f8d2d58e3c488bc8e7d
BernatBC <bernat.borrasc@gmail.com>
2024-02-05 17:49:19 +0100

    Improve light

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ const scene = new THREE.Scene()
 const ambientLight = new THREE.AmbientLight()
 scene.add(ambientLight)
 
+const light = new THREE.PointLight(0xffffff, 50)
+light.position.set(1.6, 2,8, 2.0)
+scene.add(light)
+
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.set(1.6, 2,8, 2.0)
 
