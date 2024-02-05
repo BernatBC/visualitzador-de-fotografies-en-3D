0fd8d36edfafc9f3f10c7efe980af44302373c98
BernatBC <bernat.borrasc@gmail.com>
2024-02-05 17:36:35 +0100

    Add model

--- main.js
+++ main.js
@@ -1,33 +1,51 @@
-import * as THREE from 'three';
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const scene = new THREE.Scene()
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-camera.position.z = 5;
+const ambientLight = new THREE.AmbientLight()
+scene.add(ambientLight)
 
-//Add scene content here
-addImage("images/non-square.jpeg");
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.set(1.6, 2,8, 2.0)
 
-animate();
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
 
-function addImage(path) {
-    var loader = new THREE.TextureLoader();
-    var texture = loader.load(path, function () {
-        const geometry = new THREE.PlaneGeometry( texture.image.width/200,texture.image.height/200);
-        const material = new THREE.MeshBasicMaterial( { map: texture } );
-        const plane = new THREE.Mesh( geometry, material );
-        scene.add( plane );
-    } );
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+controls.target.set(0, 2, 0)
+
+//const material = new THREE.MeshNormalMaterial()
+
+const fbxLoader = new FBXLoader()
+fbxLoader.load('models/cottage.fbx',(object) => {
+    object.scale.set(.002, .002, .002)
+    scene.add(object)
+})
+
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    render()
 }
 
 function animate() {
-	requestAnimationFrame( animate );
-	render();
+    requestAnimationFrame(animate)
+
+    controls.update()
+
+    render()
+
+    stats.update()
 }
 
 function render() {
-    renderer.render( scene, camera );
+    renderer.render(scene, camera)
 }
+
+animate()
