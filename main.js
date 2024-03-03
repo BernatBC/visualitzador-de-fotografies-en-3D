a0d166ec7ee5be865f95465bf47ef726a2048e18
BernatBC <bernat.borrasc@gmail.com>
2024-03-03 17:17:48 +0100

    Add pyramids

--- main.js
+++ main.js
@@ -77,12 +77,12 @@ out_file_loader.load( 'out-files/MNAC-AbsidiolaSud/MNAC-AbsSud-CamerasRegistrati
             const t = math.matrix(lines[line_number + 4].split(' ').map(parseFloat));
             const pos = math.multiply(math.unaryMinus(math.transpose(R)),t)
             const camera_pos = [pos.get([0]), pos.get([1]), pos.get([2])]
-            const sphere_geometry = new THREE.SphereGeometry( 0.02, 5, 5 ).translate(camera_pos[0], camera_pos[1], camera_pos[2]).rotateX(Math.PI);
+            const sphere_geometry = new THREE.SphereGeometry( 0.01, 5, 5 ).translate(camera_pos[0], camera_pos[1], camera_pos[2]).rotateX(Math.PI);
             const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } ); 
             const sphere = new THREE.Mesh( sphere_geometry, material );
             scene.add( sphere );
             //Punta de la piràmide al centre mirant cap endavant
-            const pyramid_geometry = new THREE.ConeGeometry(0.1,0.1,4).rotateY(Math.PI / 4).translate(0,-0.05,0).rotateZ(Math.PI/2)
+            const pyramid_geometry = new THREE.ConeGeometry(0.05,0.05,4).rotateY(Math.PI / 4).translate(0,-0.025,0).rotateZ(Math.PI/2)
             //Rotació
             //View direction
             const view_direction = math.multiply(math.transpose(R), math.transpose(math.matrix([0,0,-1])))
@@ -109,9 +109,9 @@ out_file_loader.load( 'out-files/MNAC-AbsidiolaSud/MNAC-AbsSud-CamerasRegistrati
             const radY = math.asin(y/h)
             pyramid_geometry.rotateZ(radY)
             //Portar-la a la posició de la càmera*
-            pyramid_geometry.translate(camera_pos[0], camera_pos[1], camera_pos[2]);
+            pyramid_geometry.translate(camera_pos[0], camera_pos[1], camera_pos[2]).rotateX(Math.PI);
             const pyramid = new THREE.Mesh(pyramid_geometry, material);
-            //scene.add(pyramid);
+            scene.add(pyramid);
 
             // Afegir imatge
             const SCALE = 1000;
