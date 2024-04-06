aa10478d70f93e1b7f5b01bef89ba080cdd9ff8e
BernatBC <bernat.borrasc@gmail.com>
2024-04-06 09:44:45 +0200

    Accomplish position images

--- interaction.js
+++ interaction.js
@@ -1,43 +1,46 @@
-import * as THREE from 'three'
-import {render} from './main.js'
-import { getImageParams } from './single-image-loader.js';
+import * as THREE from "three";
+import { render } from "./main.js";
+import { getImageParams } from "./single-image-loader.js";
+import { create, all } from "mathjs";
 
-var mouse = new THREE.Vector2()
+const math = create(all, {});
+
+var mouse = new THREE.Vector2();
 var raycaster = new THREE.Raycaster();
-var hover = undefined
+var hover = undefined;
 var mDragging = false;
 var mDown = false;
-var camera
-var scene
-var controls
+var camera;
+var scene;
+var controls;
 
-const HOVER_COLOR = 0xccffff
-const SELECTION_COLOR = 0xd6b4fc
-const NEUTRAL_COLOR = 0xffffff
+const HOVER_COLOR = 0xccffff;
+const SELECTION_COLOR = 0xd6b4fc;
+const NEUTRAL_COLOR = 0xffffff;
 
 var imagesSelected = new Set();
 
 function addInteraction(ctrl, cam, sce) {
-    controls = ctrl
-    camera = cam
-    scene = sce
+    controls = ctrl;
+    camera = cam;
+    scene = sce;
 
     // OBRIR IMATGES EN OPENSEADRAGON
-    window.addEventListener('mousedown', function () {
+    window.addEventListener("mousedown", function () {
         mDown = true;
     });
 
-    window.addEventListener('mousemove', function () {
-        if(mDown) mDragging = true;
+    window.addEventListener("mousemove", function () {
+        if (mDown) mDragging = true;
     });
 
-    window.addEventListener('mouseup', function() {
-        if(mDragging === false) onClick(camera)
+    window.addEventListener("mouseup", function () {
+        if (mDragging === false) onClick(camera);
         mDown = false;
         mDragging = false;
     });
 
-    window.addEventListener('pointermove', onHover);
+    window.addEventListener("pointermove", onHover);
 }
 
 function onClick() {
@@ -48,51 +51,56 @@ function onClick() {
     var intersects = raycaster.intersectObject(scene, true);
     if (intersects.length > 0) {
         var object = intersects[0].object;
-        if (object.name.startsWith('Sant Quirze de Pedret by Zones')) {
+        if (object.name.startsWith("Sant Quirze de Pedret by Zones")) {
             if (event.button == 0) {
-                const url = 'openseadragon.html?images=' + encodeURIComponent(JSON.stringify(object.name));
-                window.open(url, '_blank')
-                clearSelection()
-            }
-            else {
+                const url =
+                    "openseadragon.html?images=" +
+                    encodeURIComponent(JSON.stringify(object.name));
+                window.open(url, "_blank");
+                clearSelection();
+            } else {
                 if (imagesSelected.has(object)) {
-                    imagesSelected.delete(object)
-                    object.material.color.setHex( HOVER_COLOR )
-                }
-                else {
-                    imagesSelected.add(object)
-                    object.material.color.setHex( SELECTION_COLOR )
+                    imagesSelected.delete(object);
+                    object.material.color.setHex(HOVER_COLOR);
+                } else {
+                    imagesSelected.add(object);
+                    object.material.color.setHex(SELECTION_COLOR);
                 }
             }
         }
     }
-      render();
+    render();
 }
 
 function openImagesToOpenSeaDragon() {
     if (imagesSelected.size == 0) return;
-    const url = 'openseadragon.html?images=' + encodeURIComponent(JSON.stringify(imageObjectToNamesArray(imagesSelected)));
-    window.open(url, '_blank')
-    clearSelection()
+    const url =
+        "openseadragon.html?images=" +
+        encodeURIComponent(JSON.stringify(createJSON(imagesSelected)));
+    window.open(url, "_blank");
+    clearSelection();
 }
 
 function clearSelection() {
-    imagesSelected.forEach(image_object => image_object.material.color.setHex( NEUTRAL_COLOR ))
-    imagesSelected.clear()
+    imagesSelected.forEach((image_object) =>
+        image_object.material.color.setHex(NEUTRAL_COLOR)
+    );
+    imagesSelected.clear();
 }
 
 function hoverIn(image_object) {
-    if (!imagesSelected.has(image_object)) image_object.material.color.setHex( HOVER_COLOR )
-    hover = image_object
+    if (!imagesSelected.has(image_object))
+        image_object.material.color.setHex(HOVER_COLOR);
+    hover = image_object;
 }
 
 function hoverOut() {
-    if (!imagesSelected.has(hover)) hover.material.color.setHex( NEUTRAL_COLOR )
-    hover = undefined
+    if (!imagesSelected.has(hover)) hover.material.color.setHex(NEUTRAL_COLOR);
+    hover = undefined;
 }
 
 function onHover() {
-    if (mDragging) return
+    if (mDragging) return;
     event.preventDefault();
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
@@ -100,13 +108,13 @@ function onHover() {
     var intersects = raycaster.intersectObject(scene, true);
     if (intersects.length > 0) {
         var object = intersects[0].object;
-        if (object.name.startsWith('Sant Quirze de Pedret by Zones')) {
+        if (object.name.startsWith("Sant Quirze de Pedret by Zones")) {
             // New Hover
-            if (hover === undefined) hoverIn(object)
+            if (hover === undefined) hoverIn(object);
             // Replace hover
             else if (hover.name !== object.name) {
-                hoverOut()
-                hoverIn(object)
+                hoverOut();
+                hoverIn(object);
             }
         }
         // Hovering a non-image object
@@ -118,10 +126,23 @@ function onHover() {
     render();
 }
 
-function imageObjectToNamesArray(objectArray) {
-    const names = []
-    objectArray.forEach(object => names.push(object.name))
-    return names
+function createJSON(objectArray) {
+    const json = [];
+    let C = camera.position;
+
+    objectArray.forEach((object) => {
+        let P = object.position;
+        let V = new THREE.Vector3().subVectors(P, C).normalize();
+        let phi = math.acos(V.z);
+        let theta = math.atan2(V.y, V.x);
+        json.push({
+            name: object.name,
+            phi: phi,
+            theta: theta,
+        });
+    });
+    console.log(json);
+    return json;
 }
 
-export {addInteraction, openImagesToOpenSeaDragon, clearSelection}
+export { addInteraction, openImagesToOpenSeaDragon, clearSelection };
---
+++ openseadragon.js
@@ -0,0 +1,46 @@
+const urlParams = new URLSearchParams(window.location.search);
+const images = urlParams.get("images");
+const parsedImages = JSON.parse(images);
+
+var sources = [];
+var overlays = [];
+var i = 0;
+
+console.log(parsedImages);
+
+if (!Array.isArray(parsedImages)) {
+    sources.push({
+        type: "image",
+        url: "images/" + parsedImages,
+        buildPyramid: false,
+    });
+}
+
+var viewer = OpenSeadragon({
+    id: "openseadragon1",
+    prefixUrl: "openseadragon/images/",
+    tileSources: sources,
+
+    showNavigator: true,
+    preserveViewport: true,
+});
+
+if (Array.isArray(parsedImages)) {
+    parsedImages.forEach((image) => {
+        console.log(
+            "Phi: " +
+                image.phi.toString() +
+                ", Theta: " +
+                image.theta.toString()
+        );
+        viewer.addTiledImage({
+            tileSource: {
+                type: "image",
+                url: "images/" + image.name,
+                buildPyramid: false,
+            },
+            x: image.phi,
+            y: image.theta,
+        });
+    });
+}
