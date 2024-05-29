2f0b5397eec5ed9ad0f61ed6169563a01cc75b15
BernatBC <bernat.borrasc@gmail.com>
2024-05-29 17:40:48 +0200

    Add readme

--- convert_picture_list.py
+++ convert_picture_list.py
@@ -3,10 +3,10 @@
 #f = open(file_path)
 #f2 = open(destination_path)
 f = open("./public/out-files/MNAC-AbsidiolaSud/MNAC-AbsSud-CamerasList.lst", "r")
-f2 = open("./public/out-files/MNAC-AbsidiolaSud/MNAC-AbsSud-CamerasList-converted.txt", "w")
+f2 = open("./public/out-files/MNAC-AbsidiolaSud/MNAC-AbsSud-CamerasList-converted.lst", "w")
 content = f.read()
 lines = content.split('\n')
 
 for line in lines:
     words = line.split('\\')
-    f2.write('/home/bernat/Pictures/SouthAbs/' + words[len(words) - 1] + '\n')
+    f2.write('Sant Quirze de Pedret by Zones/MNAC - South Apse/' + words[len(words) - 1] + '\n')
--- reducesize.sh
+++ reducesize.sh
@@ -3,3 +3,7 @@ for f in `find . -name "*.jpg"`
 do
     convert $f -resize 10% $f
 done
+for f in `find . -name "*.JPG"`
+do
+    convert $f -resize 10% $f
+done
